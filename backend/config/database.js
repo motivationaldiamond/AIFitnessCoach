161bb6cd3ed71c9d6a23caae6a3f36eeff793445
Ieshia Parker <ieshiak@icloud.com>
2024-05-31 20:51:08 -0400

    updated database.js and keys.js

---
+++ backend/config/database.js
@@ -0,0 +1,19 @@
+//database.js
+
+const mongoose = require('mongoose');
+const { dbURI } = require('./keys');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+  dbURI: process.env.MONGO_URI,
   openaiApiKey: process.env.AZURE_OPENAI_API_KEY,
   openaiEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
 };
