97dfff19d68d5bd114e0337a8f7debc2c6564a68
Ieshia Parker <ieshiak@icloud.com>
2024-06-01 19:19:50 -0400

    added full crud to nutritionModel.js

--- backend/models/nutritionModel.js
+++ backend/models/nutritionModel.js
@@ -19,6 +19,57 @@ async function createNutrition(userId, meal, calories, date) {
     }
 }
 
+async function getNutritions() {
+    const db = getDB();
+    try {
+        const nutritions = await db.collection('nutritions').find({}).toArray();
+        return nutritions;
+    } catch (err) {
+        console.error('Error retrieving nutritions:', err);
+        throw err;
+    }
+}
+
+async function getNutritionById(id) {
+    const db = getDB();
+    try {
+        const nutrition = await db.collection('nutritions').findOne({ _id: new ObjectId(id) });
+        return nutrition;
+    } catch (err) {
+        console.error('Error retrieving nutrition:', err);
+        throw err;
+    }
+}
+
+async function updateNutrition(id, updatedNutrition) {
+    const db = getDB();
+    try {
+        const result = await db.collection('nutritions').updateOne(
+            { _id: new ObjectId(id) },
+            { $set: updatedNutrition }
+        );
+        return result.modifiedCount > 0;
+    } catch (err) {
+        console.error('Error updating nutrition:', err);
+        throw err;
+    }
+}
+
+async function deleteNutrition(id) {
+    const db = getDB();
+    try {
+        const result = await db.collection('nutritions').deleteOne({ _id: new ObjectId(id) });
+        return result.deletedCount > 0;
+    } catch (err) {
+        console.error('Error deleting nutrition:', err);
+        throw err;
+    }
+}
+
 module.exports = {
     createNutrition,
+    getNutritions,
+    getNutritionById,
+    updateNutrition,
+    deleteNutrition,
 };
