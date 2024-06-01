f3bac46d5e9b54ec61ac1b7d2372d23bc26825e8
Ieshia Parker <ieshiak@icloud.com>
2024-05-31 20:53:16 -0400

    updated userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+//userController.js
+
+const User = require('../models/userModel');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+};
+
+module.exports = { getUsers };
