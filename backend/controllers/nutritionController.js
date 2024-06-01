2c887ec806a812c813ecaa20ca2978bb8455afed
Ieshia Parker <ieshiak@icloud.com>
2024-06-01 19:10:01 -0400

    joi install and added to nutritionController

--- backend/controllers/nutritionController.js
+++ backend/controllers/nutritionController.js
@@ -1,14 +1,28 @@
 // nutritionController.js
 
 const { createNutrition } = require('../models/nutritionModel');
+const Joi = require('joi');
+
+// Define the schema for validation
+const nutritionSchema = Joi.object({
+    userId: Joi.string().required(),
+    meal: Joi.string().required(),
+    calories: Joi.number().required(),
+    date: Joi.date().required(),
+});
 
 const addNutrition = async (req, res) => {
+    const { error } = nutritionSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
     try {
         const { userId, meal, calories, date } = req.body;
         const newNutrition = await createNutrition(userId, meal, calories, date);
         res.status(201).json({ insertedId: newNutrition });
-    } catch (error) {
-        console.error('Error adding nutrition:', error);
+    } catch (err) {
+        console.error('Error adding nutrition:', err);
         res.status(500).json({ error: 'Failed to add nutrition' });
     }
 };
@@ -16,4 +30,5 @@ const addNutrition = async (req, res) => {
 module.exports = {
     addNutrition,
 };
+
 
